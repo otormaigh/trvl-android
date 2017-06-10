e82987ae614abfccd66a7ccf2c30efbb00a65272
Elliot Tormey <elliot.tormey@gmail.com>
2017-06-10 17:17:17 +0100

    update build.gradle scripts

--- app/src/androidTest/java/ie/elliot/trvl/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ie/elliot/trvl/ExampleInstrumentedTest.kt
@@ -2,6 +2,7 @@ package ie.elliot.trvl
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
+import ie.elliot.trvl.ui.BuildConfig
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,6 +20,12 @@ class ExampleInstrumentedTest {
     fun testUseAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("ie.elliot.trvl", appContext.packageName)
+        if (BuildConfig.BUILD_TYPE == "debug") {
+            assertEquals("ie.elliot.trvl.debug", appContext.packageName)
+        } else if (BuildConfig.BUILD_TYPE == "qa") {
+            assertEquals("ie.elliot.trvl.qa", appContext.packageName)
+        } else if (BuildConfig.BUILD_TYPE == "release") {
+            assertEquals("ie.elliot.trvl", appContext.packageName)
+        }
     }
 }
